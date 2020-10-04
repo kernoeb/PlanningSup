c0991a62588da9bc965b153b24fe0a274c312f6e
kernoeb <kernoeb@protonmail.com>
2020-10-04 20:02:13 +0200

    change description

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # planningiut
 
+Un planning universitaire sympathique réalisé par @kernoeb  
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/9d54a6f2-71ff-4cd6-9eea-3c9c5cf6f0b2/deploy-status)](https://app.netlify.com/sites/planningiut/deploys)
--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Un planning universitaire sympathique réalisé par @kernoeb' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
