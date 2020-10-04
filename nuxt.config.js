e140b75b497a9f3560baf91bd08925f672e1285d
kernoeb <kernoeb@protonmail.com>
2020-10-04 18:50:41 +0200

    change icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
     ]
   },
 
