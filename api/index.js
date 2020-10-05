588d54c6857c76979aebd526749e71b122a59662
kernoeb <kernoeb@protonmail.com>
2020-10-05 23:11:56 +0200

    use 3000 port

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ module.exports = app
 
 // Start standalone server if directly running
 if (require.main === module) {
-  const port = process.env.PORT || 3001
+  const port = process.env.PORT || 3000
   app.listen(port, () => {
     console.log(`API server listening on port ${port}`)
   })
