abe6fbcfd62a1823e67ad03a1197eec95d5d7dcd
kernoeb <kernoeb@protonmail.com>
2020-12-16 13:38:47 +0100

    Oups

---
+++ server/api/routes/urls.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const router = Router()
+const routeCache = require('route-cache')
+const logger = require('signale')
+const urls = require('../../../static/url.json')
+
+router.use('/urls', routeCache.cacheSeconds(60 * 60 * 24 * 7), (req, res) => {
+  logger.info('Regénération des URLs')
+  const tmpUrls = JSON.parse(JSON.stringify(urls))
+  for (const i of tmpUrls) {
+    for (const j of i.univ_edts) {
+      for (const k of j.edts) {
+        delete k.url
+      }
+    }
+  }
+  res.json(tmpUrls)
+})
+
+module.exports = router
