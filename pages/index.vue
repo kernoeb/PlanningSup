343cfdf022aff762e8bbd9ecb06a98e2c7a272d3
kernoeb <kernoeb@protonmail.com>
2021-08-30 14:02:55 +0200

    add notice

--- pages/index.vue
+++ pages/index.vue
@@ -343,6 +343,51 @@
         </template>
       </v-calendar>
     </v-sheet>
+    <v-dialog
+      v-model="modelBadUrl"
+      :overlay-color="dark ? 'white' : 'black'"
+      persistent
+      max-width="500"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          Nouveau !
+        </v-card-title>
+        <v-card-text>
+          <div style="font-size: 17px;">
+            Le planning devient <b>PlanningSup</b> !
+            <br>
+            et change de nom de domaine :
+            <br>
+            <br>
+            <v-btn style="text-transform: none" href="https://planningsup.app" outlined>
+              PlanningSup.app
+            </v-btn>
+            <br><br>
+            C'est clairement plus simple à retenir et ça permet à d'autres écoles de venir ici <small>(pas que des IUTs)</small> :)
+            <br>
+            <small>(oublie pas d'ajouter en favoris)</small>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="grey"
+            text
+            @click="modelBadUrl = false"
+          >
+            Je m'en fiche
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            href="https://planningsup.app"
+          >
+            C'est parti !
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
   <div v-else class="d-flex justify-center mt-3">
     <v-progress-circular
@@ -363,6 +408,8 @@ export default {
   middleware: 'vuetify-theme',
   data () {
     return {
+      modelBadUrl: false,
+
       // Icons
       mdiMail,
       mdiTwitter,
@@ -571,6 +618,10 @@ export default {
       this.$axios.$get(this.$config.apiUrls).then((data) => {
         this.urls = data
       }).catch(() => {})
+
+      if (document.domain === 'planningiut.herokuapp.com' || document.domain === 'planning.noewen.com') {
+        this.modelBadUrl = true
+      }
     }, 0)
 
     window.addEventListener('keyup', this.keyboard)
