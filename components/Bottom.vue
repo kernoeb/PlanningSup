45372cde03d3d65fac309fb4a7b8c3bf74373834
noewen.boisnard <kernoeb@protonmail.com>
2021-10-14 00:44:08 +0200

    ui: better bottom event info style

--- components/Bottom.vue
+++ components/Bottom.vue
@@ -2,22 +2,29 @@
   <v-bottom-sheet :value="bottom" @input="$emit('change', $event)">
     <v-sheet
       class="text-center"
-      height="200px"
     >
-      <div v-if="selectedEvent && selectedEvent.event" class="py-3">
-        <div class="mt-4 font-weight-bold ml-3 mr-3">
-          {{ selectedEvent.event.name }}
+      <div class="ml-3 mr-3 pa-2">
+        <div v-if="selectedEvent && selectedEvent.event" class="mb-2">
+          <div class="mt-4 font-weight-bold title">
+            {{ selectedEvent.event.name }}
+          </div>
+          <div v-if="selectedEvent.event.location" style="font-size: 14px; font-weight: normal" class="mb-3" :style="$vuetify.theme.dark ? 'color: gainsboro;' : null">
+            {{ selectedEvent.event.location }}
+          </div>
+          <div v-if="selectedEvent.event.description" style="font-size: 12px; font-weight: normal" class="mb-2">
+            {{ selectedEvent.event.description }}
+          </div>
+          <div class="font-weight-bold">
+            {{ $moment(selectedEvent.event.start).format('H:mm') }} - {{ $moment(selectedEvent.event.end).format('H:mm') }}
+          </div>
         </div>
-        <div v-if="selectedEvent.event.location || selectedEvent.event.description">
-          {{ selectedEvent.event.location }}{{ (selectedEvent.event.location && selectedEvent.event.description) ? ' | ' : '' }}{{ selectedEvent.event.description }}
+        <div v-if="selectedEvent && selectedEvent.content" style="font-size: 14px;">
+          <code>{{ selectedEvent.content }}</code>
         </div>
-        <div>{{ $moment(selectedEvent.event.start).format('H:mm') }} - {{ $moment(selectedEvent.event.end).format('H:mm') }}</div>
       </div>
-      <div v-if="selectedEvent && selectedEvent.content">
-        <code>{{ selectedEvent.content }}</code>
-      </div>
+      <div class="pa-5" />
       <v-btn
-        class="mt-6"
+        style="position:absolute; margin-left:-50px; left:50%; width:100px; bottom:0;"
         text
         @click="$emit('close')"
       >
