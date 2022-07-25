0da3bf6662adb9462672724879ff8e611869f3db
kernoeb <kernoeb@protonmail.com>
2022-07-25 12:23:52 +0200

    chore: update donors

--- components/DialogSettings.vue
+++ components/DialogSettings.vue
@@ -223,7 +223,7 @@
             </div>
           </v-list-item>
           <v-list-item inactive>
-            <div><small><b>Donateurs :</b> W00dy, Rick, Lahgolz, Dyskal 🙏</small></div>
+            <div><small><b>Donateurs :</b> W00dy, Rick, Lahgolz, Dyskal, Mimipepin 🙏</small></div>
           </v-list-item>
         </v-list-item-group>
       </v-card>
