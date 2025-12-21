06ff583f498dfe9d04a36231355191c4fa63f84c
kernoeb <kernoeb@protonmail.com>
2025-12-21 22:25:54 +0100

    fix: extension permissions + version

--- apps/extension/src/manifest.ts
+++ apps/extension/src/manifest.ts
@@ -1,17 +1,20 @@
 import type { Manifest } from 'webextension-polyfill'
+import type GlobalPkgType from '../../../package.json'
 import type PkgType from '../package.json'
 import fs from 'fs-extra'
 import { isDev, isFirefox, port, r } from '../scripts/utils'
 
 export async function getManifest() {
   const pkg = await fs.readJSON(r('package.json')) as typeof PkgType
+  const globalPkg = await fs.readJSON(r('../../package.json')) as typeof GlobalPkgType
+  if (!globalPkg.version) throw new Error('Missing version in global package.json')
 
   // update this file to update this manifest.json
   // can also be conditional based on your need
   const manifest: Manifest.WebExtensionManifest = {
     manifest_version: 3,
     name: pkg.displayName || pkg.name,
-    version: pkg.version,
+    version: globalPkg.version,
     description: pkg.description,
     action: {
       default_icon: 'assets/icon-512.png',
@@ -34,7 +37,6 @@ export async function getManifest() {
       128: 'assets/icon-512.png',
     },
     permissions: [
-      'tabs',
       'sidePanel',
     ],
     host_permissions: [
